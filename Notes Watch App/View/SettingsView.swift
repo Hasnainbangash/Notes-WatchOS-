484ed6f1300b43ce5e01e8b80db1483a24482b45
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-27 16:35:07 +0500

    A new swiftui file for headerview is created along with the comments for the design work done

---
+++ Notes Watch App/View/SettingsView.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsView.swift
+//  Notes Watch App
+//
+//  Created by Elexoft on 27/02/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            // HEADER
+            // ACTUAL LINE COUNT
+            // SLIDER
+        } //: VSTACK
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    SettingsView()
+}
