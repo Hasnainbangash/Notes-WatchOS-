e986b0d807df22abd53d39e28a1783ddc5e221a3
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-27 16:19:26 +0500

    Credit view design done

--- Notes Watch App/View/CreditsView.swift
+++ Notes Watch App/View/CreditsView.swift
@@ -10,11 +10,23 @@ import SwiftUI
 struct CreditsView: View {
     // MARK: - PROPERTIES
     
+    @State private var randomNumber: Int = Int.random(in: 1..<4)
+    
+    private var randomImage: String {
+        return "developer-no\(randomNumber)"
+    }
+    
     // MARK: - BODY
     
     var body: some View {
         VStack(spacing: 3) {
             // PROFILE IMAGE
+            Image(randomImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .layoutPriority(1)
+            
             // HEADER
             HeaderView(title: "Credits")
             
