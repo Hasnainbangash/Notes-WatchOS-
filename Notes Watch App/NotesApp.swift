c7d5e6abaa7a31b87f80d7cfcfc6106a2f9f6e51
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-27 11:26:09 +0500

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Elexoft on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
