2d632af7cae88e07a668263451f2f0cc932dd10e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-27 15:59:08 +0500

    A NEW FILE CREDITS VIEW IS CREATED

---
+++ Notes Watch App/View/CreditsView.swift
@@ -0,0 +1,24 @@
+//
+//  CreditsView.swift
+//  Notes Watch App
+//
+//  Created by Elexoft on 27/02/2025.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    CreditsView()
+}
