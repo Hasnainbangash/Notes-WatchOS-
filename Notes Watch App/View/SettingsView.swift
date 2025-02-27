c3fbcdd0289a042ea751524cbe5e105e5965c41a
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-27 17:01:24 +0500

    Update function is created and the slider is modified using the binding of set and get functions buldin in binding

--- Notes Watch App/View/SettingsView.swift
+++ Notes Watch App/View/SettingsView.swift
@@ -13,6 +13,12 @@ struct SettingsView: View {
     @AppStorage("lineCount") var lineCount: Int = 1
     @State private var value: Float = 1.0
     
+    // MARK: - FUNCTION
+    
+    func update() {
+        lineCount = Int(value)
+    }
+    
     // MARK: - BODY
     
     var body: some View {
@@ -25,9 +31,13 @@ struct SettingsView: View {
                 .fontWeight(.bold)
             
             // SLIDER
-            Slider(value: $value, in: 1...4, step: 1)
+            Slider(value: Binding(get: {
+                self.value
+            }, set: { newValue in
+                self.value = newValue
+                self.update()
+            }), in: 1...4, step: 1)
                 .accentColor(.accentColor)
-            
         } //: VSTACK
     }
 }
